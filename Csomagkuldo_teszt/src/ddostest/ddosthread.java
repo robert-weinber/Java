b490195ae3ff26351eb3d989c0cf12e3a134463a
Robert Weinber <robert.weinber@gmail.com>
2020-01-20 22:31:46 +0100

    Cleanup

--- Csomagkuldo_teszt/src/ddostest/ddosthread.java
+++ Csomagkuldo_teszt/src/ddostest/ddosthread.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ddostest;
 
 import java.io.IOException;
@@ -26,7 +21,6 @@ import java.util.logging.Logger;
 
 public class ddosthread extends Thread {
 
-       // private AtomicBoolean running = new AtomicBoolean(false);
         byte[] buffer = {10,23,12,31,43,32,24};
         byte [] IP={-64,-88,1,106};
         int port=57;
@@ -37,7 +31,7 @@ public class ddosthread extends Thread {
         int count=0;
         int ThreadID;
         List<Socket> sockets = new ArrayList<Socket>();
-Socket s = new Socket();
+		Socket s = new Socket();
         String param = null;
 
         public ddosthread(int id, byte[] buff, byte[] ip, int port, long frekvencia) {
@@ -53,17 +47,12 @@ Socket s = new Socket();
             }
         }
 
-
         @Override
         public void run() {
                 try {
                     sendIT();
                 } catch (Exception e) {
-
                 }
-
-
-            
         }
         
         public void stopIT(){
@@ -83,47 +72,17 @@ Socket s = new Socket();
         public void sendIT() throws Exception {
             senderTimer = new Timer();
         InetAddress address = InetAddress.getByAddress(IP);
-        //DatagramPacket 
-            /*    packet = new DatagramPacket(
-                buffer, buffer.length, address, port
-                );*/
             senderTimer.scheduleAtFixedRate(new TimerTask() {
   @Override
   public void run() {
-      //for (int i = 0; i < frekvencia; i++) {
-          
-     //Socket s = new Socket();
-    // sockets.add(s);
-          //System.out.println(ThreadID+": "+sockets.size());
       try {
           for (int i = 0; i < 100; i++) {              
           Socket s = new Socket(); 
           s.connect(new InetSocketAddress(address, 12000), 1000);
           }
-          //s.close();
-      } catch (IOException ex) {
-          //Logger.getLogger(ddosthread.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      //}
-  }
-}, 300, 1000);
-        
-        //DatagramSocket datagramSocket = new DatagramSocket();
-        
-        
-       /* senderTimer.scheduleAtFixedRate(new TimerTask() {
-  @Override
-  public void run() {
-      try {
-          for (int i = 0; i < 10; i++) {
-          datagramSocket.send(packet);
-          count++;
-          System.out.println(ThreadID+". szál: "+count);
-          }
-      } catch (IOException ex) {
-          Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
-      }
-  }
-}, 100, frekvencia);*/
-        }
+						} catch (IOException ex) {
+					}
+				}
+			}, 300, 1000);        
+		}
     }
